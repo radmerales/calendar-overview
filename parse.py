7e8ab4c3798b27bf7d0970d6efebc07df4a2a8af
radmerales <radmerales12345@gmail.com>
2025-02-18 21:50:14 +0800

    added readme file

--- parse.py
+++ parse.py
@@ -8,10 +8,10 @@ class Calendar():
         self.cal = []
         r = requests.get(link)
         decoded_events = r.content.decode("utf-8").split("\n")
-        
+
 
 class Event():
-    def __init__(self):
+    def __init__(self, ):
         'BEGIN:VEVENT\r', 'DTSTART;TZID=Asia/Manila:20230922T130000\r', 'DTEND;TZID=Asia/Manila:20230922T143000\r', 'DTSTAMP:20250218T124103Z\r', 'UID:5uq08lf4sv9afu2boa7893305m@google.com\r', 'RECURRENCE-ID;TZID=Asia/Manila:20230922T130000\r', 'CREATED:20230908T140514Z\r', 'LAST-MODIFIED:20240119T015938Z\r', 'SEQUENCE:0\r', 'STATUS:CONFIRMED\r', 'SUMMARY:KAS 1 WFW1 - asynch\r', 'TRANSP:OPAQUE\r', 'END:VEVENT\r'
 
 # "https://calendar.google.com/calendar/ical/3q8fom4rgsrc69lnrsths0d1jg%40group.calendar.google.com/private-c54511e0a53a418854721c0deccdce26/basic.ics
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Calendar Overview EInk
+
+The project shows the current day calendar view on a 3.7 inch e-paper screen using a Raspberry Pico W. It will need to download the `.ics` file and preprocess it to be readable using basic python(so that it can run in micropython). 
+
+ - [ ] Parse ICS calendar file to OOP
+ - [ ] Use basic API for the weather and time function
+ - [ ] Print information to the screen
+ - [ ] Develop a UI for the web also
